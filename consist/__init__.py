b472b2666b42d3efb6e9004a8726de8997b8eefc
ArafathUIU <arafath@uiu.ac.bd>
2026-07-26 10:09:40 +0600

    Fix __init__.py imports: remove nonexistent BiasDomain and IntersectionalGroup

--- consist/__init__.py
+++ consist/__init__.py
@@ -1,19 +1,21 @@
-from .config import CONSISTConfig, GroupConfig, BiasDomain
-from .prompts import PromptGenerator, IntersectionalGroup, BiasDomain
+from .config import CONSISTConfig, GroupConfig, INTERSECTIONAL_GROUPS, CDS_DISTANCE_METRICS
+from .prompts import PromptGenerator, PromptPair, PromptSet, BIAS_DOMAINS, TEMPLATES
 from .generate import GenerationHarness
 from .embed import EmbeddingExtractor
-from .cds import CDSCalculator, CDSResult
+from .cds import CDSCalculator, CDSResult, PairCDS
 from .stats import StatisticalAnalyzer
-from .validate import ValidationSuite
-from .finetune import FineTuningIntervention
+from .validate import ValidationSuite, ValidationReport
+from .finetune import FineTuningIntervention, InterventionResult
+from .pipeline import CONSISTPipeline
 
 __all__ = [
-    "CONSISTConfig", "GroupConfig", "BiasDomain",
-    "PromptGenerator", "IntersectionalGroup", "BiasDomain",
+    "CONSISTConfig", "GroupConfig", "INTERSECTIONAL_GROUPS", "CDS_DISTANCE_METRICS",
+    "PromptGenerator", "PromptPair", "PromptSet", "BIAS_DOMAINS", "TEMPLATES",
     "GenerationHarness",
     "EmbeddingExtractor",
-    "CDSCalculator", "CDSResult",
+    "CDSCalculator", "CDSResult", "PairCDS",
     "StatisticalAnalyzer",
-    "ValidationSuite",
-    "FineTuningIntervention",
+    "ValidationSuite", "ValidationReport",
+    "FineTuningIntervention", "InterventionResult",
+    "CONSISTPipeline",
 ]
